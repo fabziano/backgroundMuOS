dd92e95179ab91a37203eb922a87c83727667b7a
Fabiano <fabianotmp@gmail.com>
2024-06-07 21:49:35 -0300

    console 2

--- assets/script/console2.js
+++ assets/script/console2.js
@@ -1,9 +1,4 @@
-const nomeFoto = document.getElementById("nomeFoto");
 const botaoColarFoto = document.getElementById("botaoColarFoto");
-const jogo = document.getElementById("jogo");
-const botaoConverterParaPNG = document.getElementById("converterParaPNG");
-
-botaoConverterParaPNG.addEventListener("click", salvarComoPNG);
 
 const removerBackgroundBtn = document.getElementById("removerBackgroundBtn");
 removerBackgroundBtn.addEventListener("click", removerBackground);
@@ -56,38 +51,49 @@ function changeImage() {
     var image = document.getElementById("capaJogo");
 
     var gameImages = {
-        "arcade": "assets/img/consoles/arcade/console.png",
-        "atari2600": "assets/img/consoles/atari2600/console.png",
-        "atarist": "assets/img/consoles/atarist/console.png",
-        "cps": "assets/img/consoles/cps1/console.png",
-        "famicom": "assets/img/consoles/famicom/console.png",
-        "gameandwatch": "assets/img/consoles/gameandwatch/console.png",
-        "gamegear": "assets/img/consoles/gamegear/console.png",
-        "gb": "assets/img/consoles/gb/console.png",
-        "gba": "assets/img/consoles/gba/console.png",
-        "gbc": "assets/img/consoles/gbc/console.png",
-        "mame": "assets/img/consoles/mame/console.png",
-        "dreamcast": "assets/img/consoles/dreamcast/console.png",
-        "mastersystem": "assets/img/consoles/mastersystem/console.png",
-        "megacd": "assets/img/consoles/megacd/console.png",
-        "megadrive": "assets/img/consoles/megadrive/console.png",
-        "n64": "assets/img/consoles/n64/console.png",
-        "nds": "assets/img/consoles/nds/console.png",
-        "neogeo": "assets/img/consoles/neogeo/console.png",
-        "nes": "assets/img/consoles/nes/console.png",
-        "ngpc": "assets/img/consoles/ngpc/console.png",
-        "pcengine": "assets/img/consoles/pcengine/console.png",
-        "ports": "assets/img/consoles/ports/console.png",
-        "psp": "assets/img/consoles/psp/console.png",
-        "psx": "assets/img/consoles/psx/console.png",
-        "sega32x": "assets/img/consoles/sega32x/console.png",
-        "segacd": "assets/img/consoles/segacd/console.png",
-        "sfc": "assets/img/consoles/sfc/console.png",
-        "snes": "assets/img/consoles/snes/console.png",
-        "supergrafx": "assets/img/consoles/supergrafx/console.png",
-        "virtualboy": "assets/img/consoles/virtualboy/console.png",
-        "wonderswancolor": "assets/img/consoles/wonderswancolor/console.png",
+        "amstradcpc": "assets/img/wheels/amstradcpc.png",
+        "apps": "assets/img/wheels/apps.png",
+        "arcade": "assets/img/wheels/arcade.png",
+        "atari2600": "assets/img/wheels/atari2600.png",
+        "atari5200": "assets/img/wheels/atari5200.png",
+        "atari7800": "assets/img/wheels/atari7800.png",
+        "atarilynx": "assets/img/wheels/atarilynx.png",
+        "colecovision": "assets/img/wheels/colecovision.png",
+        "cps1": "assets/img/wheels/cps1.png",
+        "cps2": "assets/img/wheels/cps2.png",
+        "cps3": "assets/img/wheels/cps3.png",
+        "daphne": "assets/img/wheels/daphne.png",
+        "ports": "assets/img/wheels/ports.png",
+        "gb": "assets/img/wheels/gb.png",
+        "gba": "assets/img/wheels/gba.png",
+        "gbc": "assets/img/wheels/gbc.png",
+        "msx": "assets/img/wheels/msx.png",
+        "nds": "assets/img/wheels/nds.png",
+        "neogeo": "assets/img/wheels/neogeo.png",
+        "neogeocd": "assets/img/wheels/neogeocd.png",
+        "ngpc": "assets/img/wheels/ngpc.png",
+        "n64": "assets/img/wheels/n64.png",
+        "nes": "assets/img/wheels/nes.png",
+        "openbor": "assets/img/wheels/openbor.png",
+        "pcengine": "assets/img/wheels/pcengine.png",
+        "pce-cd": "assets/img/wheels/pce-cd.png",
+        "psp": "assets/img/wheels/psp.png",
+        "psx": "assets/img/wheels/psx.png",
+        "sega32x": "assets/img/wheels/sega32x.png",
+        "segacd": "assets/img/wheels/segacd.png",
+        "gamegear": "assets/img/wheels/gamegear.png",
+        "mastersystem": "assets/img/wheels/mastersystem.png",
+        "megadrive": "assets/img/wheels/megadrive.png",
+        "naomi": "assets/img/wheels/naomi.png",
+        "sg-1000": "assets/img/wheels/sg-1000.png",
+        "x68000": "assets/img/wheels/x68000.png",
+        "zxspectrum": "assets/img/wheels/zxspectrum.png",
+        "snes": "assets/img/wheels/snes.png",
+        "tic80": "assets/img/wheels/tic80.png",
+        "vectrex": "assets/img/wheels/vectrex.png",
+        "wonderswancolor": "assets/img/wheels/wonderswancolor.png"
     };
+    
 
     if (selectedConsole in gameImages) {
         image.src = gameImages[selectedConsole];
@@ -96,29 +102,35 @@ function changeImage() {
     }
 }
 
-function salvarComoPNG() {
-    const nomeArquivo = nomeFoto.value.trim() || document.getElementById("consoleGames").value;
+const jogo = document.getElementById("jogo");
+const nomeFoto = document.getElementById("nomeFoto");
+const botaoConverterParaPNG = document.getElementById("converterParaPNG");
 
-    html2canvas(jogo, {
-        width: 640,
-        height: 480,
-        scale: 1,
-        backgroundColor: null 
-    }).then((canvas) => {
-        canvas.toBlob(function(blob) {
-            if (blob) {
-                const url = URL.createObjectURL(blob);
+botaoConverterParaPNG.addEventListener("click", () => salvarComoPNG(jogo, nomeFoto));
 
-                const link = document.createElement('a');
-                link.href = url;
-                link.download = nomeArquivo + '.png';
+nomeFoto.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    salvarComoPNG(jogo, nomeFoto);
+  }
+});
 
-                link.click();
+function salvarComoPNG(element, input) {
+  const nomeArquivo = input.value.trim() || "Imagem";
+  const rect = element.getBoundingClientRect();
 
-                URL.revokeObjectURL(url);
-            } else {
-                console.error('Erro ao gerar o blob da imagem.');
-            }
-        }, 'image/png');
-    });
+  domtoimage.toBlob(element, {
+    width: rect.width,
+    height: rect.height,
+    left: rect.left,
+    top: rect.top
+  })
+  .then(blob => {
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = `${nomeArquivo}.png`;
+    link.click();
+  })
+  .catch(error => {
+    console.error("Erro ao salvar como PNG: ", error);
+  });
 }
